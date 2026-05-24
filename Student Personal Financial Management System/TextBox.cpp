cb4dcb555476aeeb4a3df137dc27c5eb7872fab3
Leo <2242459096@qq.com>
2026-05-24 14:55:12 +0800

    增加TextBox输入光标和键盘相关键位识别

--- Student Personal Financial Management System/TextBox.cpp
+++ Student Personal Financial Management System/TextBox.cpp
@@ -6,6 +6,13 @@ void TextBox::show() {
 	::setfillcolor(m_currentColor);
 	::fillroundrect(m_x, m_y, m_x + m_w, m_y + m_h, 10, 10);
 
+	// 处理插入符闪烁
+	clock_t currentTime = clock();
+	if (currentTime - m_lastCaretBlinkTime > 500) { // 每500ms闪烁一次
+		m_lastCaretBlinkTime = currentTime;
+		m_showCaret = !m_showCaret;
+	}
+
 	Text displayText = m_text;
 	if (isPassword) {
 		std::wstring temp(m_text.getText().size() - 1, L'*');
@@ -18,6 +25,7 @@ void TextBox::show() {
 	
 	displayText.setPosition(tx, ty);
 	displayText.show();
+	drawCaret();
 }
 
 void TextBox::eventLoop(const ExMessage& msg) {
@@ -26,10 +34,41 @@ void TextBox::eventLoop(const ExMessage& msg) {
 		if (isHover()) {
 			isActive = true;
 			m_currentColor = m_clickColor;
+			m_showCaret = true;
+			m_lastCaretBlinkTime = clock();// 重置闪烁计时器
+			
+			// 取点击位置
+			int clickX = m_msg.x;
+			if (clickX >= getCaretPositionX(m_text.getText().size())) {
+				updateCaretPosition(m_text.getText().size());
+			}
+			else {
+				int posBefore = 0;
+				while (posBefore < m_text.getText().size()) {
+					int caretTempX = getCaretPositionX(posBefore+1);
+					if (caretTempX >= clickX)break;
+					posBefore++;
+				}
+				int posAfter = posBefore + 1;
+				if (isPassword) {
+					if ((clickX - getCaretPositionX(posBefore)) < (::textwidth(_T('*')) / 2)) {
+						updateCaretPosition(posBefore);
+					}
+					else updateCaretPosition(posAfter);
+				}
+				else {
+					if ((clickX - getCaretPositionX(posBefore)) < (::textwidth(m_text.getText()[posBefore]) / 2)) {
+						updateCaretPosition(posBefore);
+					}
+					else updateCaretPosition(posAfter);
+				}
+			}
+			
 		}
 		else {
 			isActive = false;
 			m_currentColor = m_defaultColor;
+			m_showCaret = false;
 		}
 	}
 
@@ -38,16 +77,32 @@ void TextBox::eventLoop(const ExMessage& msg) {
 	if (msg.message == WM_CHAR) {
 		wchar_t ch = (wchar_t)msg.ch;
 		if (ch >= 32 && ch != 127) {
-			std::wstring temp = m_text.getText();
-			temp += ch;
-			m_text.setText(temp);
+			insertCharacter(ch);
 		}
 	}
 	if (msg.message == WM_KEYDOWN) {
-		if (msg.vkcode == VK_BACK && !m_text.getText().empty()) {
-			std::wstring temp = m_text.getText();
-			temp.pop_back();
-			m_text.setText(temp);
+		switch (msg.vkcode) {
+		case VK_BACK:
+			deleteCharacter();
+			break;
+		case VK_LEFT:
+			moveCaretLeft();
+			break;
+		case VK_RIGHT:
+			moveCaretRight();
+			break;
+		case VK_DELETE:
+			if (m_caretPosition < (int)m_text.getText().size()) {
+				moveCaretRight();
+				deleteCharacter();
+			}
+			break;
+		case VK_HOME:
+			updateCaretPosition(0);
+			break;
+		case VK_END:
+			updateCaretPosition(m_text.getText().size());
+			break;
 		}
 	}
 }
@@ -62,4 +117,55 @@ void TextBox::setTextFont(const std::wstring& fontName) {
 
 void TextBox::setTextSize(int height, int width) {
 	m_text.setFixedSize(height, width);
+}
+
+void TextBox::drawCaret() {
+	if (!isActive || !m_showCaret)return;
+
+	int caretX = getCaretPositionX(m_caretPosition);
+	int caretY = m_y + (m_h - ::textheight(_T("A"))) / 2; // 使用"A"的高度作为基准
+
+	::setlinecolor(WHITE);
+	::setlinestyle(PS_SOLID);
+	::line(caretX, caretY, caretX, caretY + ::textheight(_T("A")));
+}
+
+int TextBox::getCaretPositionX(int pos) {
+	std::wstring fullText = m_text.getText();
+	std::wstring textBeforeCaret = fullText.substr(0, pos);
+	if (isPassword) {
+		textBeforeCaret = std::wstring(textBeforeCaret.size(), _T('*'));
+	}
+	int caretX = m_x + 5 + ::textwidth(textBeforeCaret.c_str());
+	return caretX;
+}
+
+void TextBox::updateCaretPosition(int newPos) {
+	if (newPos < 0)m_caretPosition = 0;
+	if (newPos > m_text.getText().size())m_caretPosition = m_text.getText().size();
+	else m_caretPosition = newPos;
+}
+
+void TextBox::moveCaretLeft() {
+	updateCaretPosition(m_caretPosition - 1);
+}
+
+void TextBox::moveCaretRight() {
+	updateCaretPosition(m_caretPosition + 1);
+}
+
+void TextBox::insertCharacter(wchar_t ch) {
+	std::wstring temp = m_text.getText();
+	temp.insert(m_caretPosition, 1, ch);
+	m_text.setText(temp);
+	updateCaretPosition(m_caretPosition + 1);
+}
+
+void TextBox::deleteCharacter() {
+	if (m_caretPosition > 0) {
+		std::wstring temp = m_text.getText();
+		temp.erase(m_caretPosition - 1, 1);
+		m_text.setText(temp);
+		updateCaretPosition(m_caretPosition - 1);
+	}
 }
--- Student Personal Financial Management System/TextBox.h
+++ Student Personal Financial Management System/TextBox.h
@@ -4,6 +4,7 @@
 #include "Text.h"
 #include <string>
 #include <ctime>
+#include <algorithm>
 class TextBox :
     public BasicWidget {
 private:
@@ -14,6 +15,10 @@ private:
     COLORREF m_defaultColor = WHITE;
 	COLORREF m_clickColor = RGB(255, 0, 0);
     COLORREF m_currentColor = WHITE;
+
+	bool m_showCaret = false; // 是否显示插入符
+	clock_t m_lastCaretBlinkTime = 0; // 上次插入符闪烁的时间
+	int m_caretPosition = 0; // 字符索引前，即0表示在第一个字符前,可以取值到文本长度，即指向末尾
 public:
     TextBox(int x=0, int y=0, int w=0, int h=0, bool isPassword = false);
     void show()override;
@@ -22,5 +27,14 @@ public:
     void setTextColor(COLORREF color);
     void setTextFont(const std::wstring& fontName);
     void setTextSize(int height, int width = 0);
+
+private:
+    void drawCaret();  // 绘制插入符
+	int getCaretPositionX(int pos); // 获取插入符在指定位置的x坐标
+    void updateCaretPosition(int newPos);  // 更新插入符位置
+    void moveCaretLeft();   // 插入符左移
+    void moveCaretRight();  // 插入符右移
+    void insertCharacter(wchar_t ch);  // 插入字符
+    void deleteCharacter();  // 删除字符
 };
 
